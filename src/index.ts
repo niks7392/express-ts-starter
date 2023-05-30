34b6df7f29a8a847b55471cec6c9c4699b739253
nikant7392 <nikantkamat99@gmail.com>
2023-05-30 21:39:23 +0530

    reset

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,10 @@
 import express, { Application } from 'express';
-import config from './config';
-import middlewares from './middlewares';
-import database from './database';
-import Socket from './websocket';
 const app: Application = express();
-
-// database connectgion 
-database(app);
-
-// application middlewares
-app.use(middlewares);
-
-let ws = new Socket(config.marketDataFeedWsUrl, config.bearerAccessToken)
-ws.run()
-ws.ws?.on('open',(e:any)=>{
-    console.log('connected');
-    
-})
 
 
 
 app.on('ready', ()=>{
-    app.listen(config.port, (): void => {
+    app.listen('', (): void => {
         console.log(`Congrats your typescript application running on http://${config.host}:${config.port}`);
     })
 })
--- src/middlewares.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import morgan from "morgan";
-
-const router = Router();
-
-
-router.use(morgan('combined'));
-
-export default router;
