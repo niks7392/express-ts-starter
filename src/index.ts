ba82cf5ec1bdfb5cfea11a37de33410fb51de4f7
nikant <nikantkamat99@gmail.com>
2023-08-03 18:57:19 +0530

    unable to read other rouutes

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 
 import express, { Request, Response } from "express"
 import config from "./config"
+import serveStatic from "serve-static";
 const app = express()
 
 
-if(process.env.NODE_ENV==='production'){
-    app.use('/', express.static('client/dist'));
-};
-app.get('/test', (req: Request, res: Response) => {
-    res.send('ok').end();
-})
 
+const STATIC_PATH =
+  process.env.NODE_ENV === "production"
+    ? `${process.cwd()}/client/dist`
+    : `${process.cwd()}/client/`;
+// app.get('/test', (req: Request, res: Response) => {
+//     res.send('ok').end();
+// })
+console.log(STATIC_PATH);
 
+app.use(serveStatic(STATIC_PATH, { index: false }));
 app.listen(config.PORT, () => console.log(`Application is running on ${config.HOST}`));
